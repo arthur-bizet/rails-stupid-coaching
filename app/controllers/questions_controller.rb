00b518d7a5791eca2d471e74fc830c4f5d107769
Arthur Bizet <arthur.m.bizet@gmail.com>
2020-08-10 12:43:23 +0200

    yes

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,5 @@ class QuestionsController < ApplicationController
 
   def answer
     @answer = ["Great!", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
-
-    if params[:question] == "I am going to work"
-      @answer[0]
-    elsif params[:question].include?("?")
-      @answer[1]
-    else
-      @answer[2]
-    end
   end
 end
